49a3d1776422934038f9cee8c2d5649d860d06fb
Joyce Siqueira <joycitta@gmail.com>
2023-03-18 11:21:31 -0300

    Add Ex01

---
+++ Lista1_01.c
@@ -0,0 +1,9 @@
+//O seu primeiro programa em qualquer linguagem de programação normalmente é
+//o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir
+//esta mensagem na tela
+
+#include<stdio.h>
+
+int main(){
+    printf("Hello World!");
+}
--- README.md
+++ README.md
@@ -7,5 +7,3 @@
 $ git config --global user.name "John Doe"
 $ git config --global user.email johndoe@example.com
 ```
-
-ALTERAÇÃO TESTE GITHUB
