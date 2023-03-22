913b31324a772373fd720d3590f48aefcef4bae3
Joyce Siqueira <joycitta@gmail.com>
2023-03-21 21:03:29 -0300

    Lista 01 Ex 03

---
+++ Lista01_03.c
@@ -0,0 +1,33 @@
+/*
+Faça um algoritmo que leia um numero inteiro, dois números reais (um float, com 3
+casas depois da vírgua, e um double), um caracter (uma única letra), uma string
+(conjunto de caracteres) e um endereço de memória, em hexadecimal. Na
+sequência, imprima todos com a formatação abaixo.
+*/
+
+#include<stdio.h>
+int main(){
+    int numeroInteiro;
+    float numeroRealFloat;
+    double numeroRealDouble;
+    char umaLetra;
+    char umaString[100];
+
+    scanf("%d", &numeroInteiro);
+    scanf("%f", &numeroRealFloat);
+    scanf("%lf", &numeroRealDouble);
+    scanf("%c", &umaLetra);
+    
+    scanf("%s", &umaString);
+    gets(umaString);
+    fgets(umaString, 100, stdin);
+
+    printf("%3d: Inteiro\n", numeroInteiro);
+    printf("Float: %.3f\n", numeroRealFloat);
+    printf("Double: %.1lf\n", numeroRealDouble);
+    printf("Char: %c\n", umaLetra);
+    printf("String: %s\n", umaString);
+    printf("Endereco do Inteiro %x", &numeroInteiro);
+
+
+}
