d1f3a98b4652b1825d8e43046ff36af78bbb9de3
Joyce Siqueira <joycitta@gmail.com>
2023-03-21 21:02:46 -0300

    Lista 01 Ex 02

---
+++ Lista1_02.c
@@ -0,0 +1,22 @@
+/*
+Caderno de poesias é um belo lugar.
+Tantas coisas lindas que eu gostaria de falar.
+Eu falo em forma de versos para todos poderem escutar.
+Agora você já sabe por que os poetas passam os dias escrevendo em seus
+cadernos de poesias. 
+*/
+
+#include<stdio.h>
+
+int main(){
+
+printf("Caderno de poesias é um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\n
+Eu falo em forma de versos para todos poderem escutar.\nAgora você já sabe por que os poetas passam os dias escrevendo em seus
+cadernos de poesias.");
+
+printf("Caderno de poesias é um belo lugar.\n");
+printf("Tantas coisas lindas que eu gostaria de falar.\n");
+printf("Eu falo em forma de versos para todos poderem escutar.\n");
+printf("Agora você já sabe por que os poetas passam os dias escrevendo em seus\ncadernos de poesias.");
+
+}
