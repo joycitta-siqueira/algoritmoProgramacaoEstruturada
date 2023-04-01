5caca0e319236f3979526079253dfe74b65dd1f0
Joyce Siqueira <joycitta@gmail.com>
2023-04-01 08:34:50 -0300

    add

--- Lista1_01.c
+++ Lista1_01.c
@@ -1,6 +1,6 @@
 //O seu primeiro programa em qualquer linguagem de programação normalmente é
 //o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir
-//esta mensagem na tela
+//esta mensagem na tela 
 
 #include<stdio.h>
 
